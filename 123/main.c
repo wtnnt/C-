f351253bb9e6fb9d1f98d37ac8c27cc208b32b10
wtnnt <koryak.ink.bg@gmail.com>
2025-02-15 14:11:22 +1000

    Update main.c

--- 123/main.c
+++ 123/main.c
@@ -4,8 +4,5 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	print("Hello world"):
-	
 	return 0;
 }
